d06de0b68951768366052a88281e9af89e60f221
digitalappsbd <sufibd2010@gmail.com>
2022-09-22 07:49:16 +0600

    changed mongodb credentials

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.menvh.mongodb.net/db`;
+// const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.vdzhznl.mongodb.net/test`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.vdzhznl.mongodb.net`;
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
 
